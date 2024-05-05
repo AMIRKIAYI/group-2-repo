14a49f3b5e3caf363ac5a2ad883731a207b22a26
AMIRKIAYI <amirkiayi516@gmail.com>
2024-05-05 12:35:13 +0300

    update index.js

---
+++ switch.js
@@ -0,0 +1,43 @@
+//Switch is a statement used to perform different actions based on different conditions.
+
+
+
+let months = 4;
+switch(months){
+    case 1:
+        console.log("January");
+        break;
+        case 2:
+            console.log("February");
+            break;
+            case 3:
+                console.log("March");
+                break;
+                case 4:
+                    console.log("April");
+                    break;
+                    case 5:
+                        console.log("May");
+                        break;
+                        default:
+                            console.log("none of the above");
+}
+
+
+let grade = 80;
+switch(grade){
+    case 90:
+        case 80:
+            case 70:
+                console.log("You passed");
+                break;
+                case 60:
+                    case 50:
+                        case 40:
+                            console.log("You are average");
+                            break;
+                            case 30:
+                                case 20:
+                                    case 10:
+                                        console.log("You failed")
+}
